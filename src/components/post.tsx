725ad4a826f7f10019b626c840907d1d093ab148
7eppelin <antipvp@gmail.com>
2022-10-27 11:03:41 +0300

    refactor post component

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,15 +1,28 @@
 import { FC } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import { Post as PostType, User } from "src/types";
 import { formatDateString } from "src/utils";
 
-const PostWrapper = styled.article``;
+import { Link } from "./link";
+import { PALETTE } from "src/constants";
 
-const LinkStyled = styled(Link)`
+const PostWrapper = styled.article`
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+`;
+
+const PostInfo = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
 	font-size: 12px;
 	color: #ccc;
+
+	a {
+		color: ${PALETTE.grey};
+	}
 `;
 
 type Props = {
@@ -20,17 +33,22 @@ type Props = {
 export const Post: FC<Props> = ({ post, author }) => {
 	const { id, title, body, date } = post;
 
-	const lastEdited = formatDateString(date);
-
 	return (
 		<PostWrapper>
 			<h2>{title}</h2>
 
-			<div>
-				{author && <span>By {author.name}</span>}
-				{"last updated: "}
-				<LinkStyled to={`/posts/${id}`}>{lastEdited}</LinkStyled>
-			</div>
+			<PostInfo>
+				{author && (
+					<span>
+						By <Link to={`/${author.id}`}>{author.name}</Link>
+					</span>
+				)}
+
+				<div>
+					<span>{"last updated: "}</span>
+					<Link to={`/posts/${id}`}>{formatDateString(date)}</Link>
+				</div>
+			</PostInfo>
 
 			<p>{body}</p>
 		</PostWrapper>
